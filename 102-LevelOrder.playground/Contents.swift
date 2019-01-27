a96d48686bece74bdc8b46308d5ef3a643d231bd
William <cavalry001@gmail.com>
2019-01-27 17:10:37 +0800

    Add new resolution
    Add new resolution, unfinished!

---
+++ 102-LevelOrder.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+
+class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init(_ val: Int) {
+        self.val = val
+    }
+}
+
+
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        
+    }
+}
