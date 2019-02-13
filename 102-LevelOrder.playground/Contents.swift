7aeb1ef7cde2aaa39d978834a3b01a1d6eecd13b
Dracarys <cavalry001@gmail.com>
2019-02-13 16:57:57 +0800

    new solution

--- 102-LevelOrder.playground/Contents.swift
+++ 102-LevelOrder.playground/Contents.swift
@@ -14,6 +14,33 @@ class TreeNode {
 
 class Solution {
     func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let r = root else { return [] }
         
+        
+        var result = [[Int]]()
+        
+        appendLevelNode(result: &result, nodes: [r])
+        
+        return result
+    }
+    
+    func appendLevelNode(result: inout [[Int]], nodes: [TreeNode]){
+        guard nodes.count > 0 else { return }
+        
+        var curLevelResult = [Int]()
+        var nextLevelNodes = [TreeNode]()
+        for node in nodes {
+            if let l = node.left {
+                nextLevelNodes.append(l)
+                curLevelResult.append(l.val)
+            }
+            if let r = node.right {
+                nextLevelNodes.append(r)
+                curLevelResult.append(r.val)
+            }
+        }
+        result.append(curLevelResult)
+
+        appendLevelNode(result: &result, nodes: nextLevelNodes)
     }
 }
