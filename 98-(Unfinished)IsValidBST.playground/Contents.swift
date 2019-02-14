2fbe1e10a34e8489f7c666b7aef6dfcfd857e7b9
Dracarys <cavalry001@gmail.com>
2019-02-14 16:56:48 +0800

    update resolution

--- 98-(Unfinished)IsValidBST.playground/Contents.swift
+++ 98-(Unfinished)IsValidBST.playground/Contents.swift
@@ -14,27 +14,22 @@ class Solution {
     func isValidBST(_ root: TreeNode?) -> Bool {
         guard let root = root else { return true }
         
-        if !isValidLeftOfBST(root, left: root.left) || !isValidRightOfBST(root, right: root.right) {
-            return false;
+        if let l = root.left {
+            if !isValidBST(l) {
+                return false
+            } else if l.val >= root.val{
+                return false
+            }
         }
-        return isValidBST(root.left) && isValidBST(root.right)
-    }
-    
-    func isValidLeftOfBST(_ root: TreeNode?, left: TreeNode? ) -> Bool {
-        guard let root = root else { return true }
         
-        if let left = left, left.val >= root.val {
-            return false;
+        if let r = root.right {
+            if !isValidBST(r) {
+                return false
+            } else if r.val <= root.val{
+                return false
+            }
         }
-        return true
-    }
-    
-    func isValidRightOfBST(_ root: TreeNode?, right: TreeNode? ) -> Bool {
-        guard let root = root else { return true }
         
-        if let right = right, right.val <= root.val {
-            return false;
-        }
         return true
     }
 }
