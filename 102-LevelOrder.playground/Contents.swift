ddbeea8c72f8315635d0fdf30e507d8b030427d2
Dracarys <cavalry001@gmail.com>
2019-02-14 15:59:24 +0800

    Add new resolution

--- 102-LevelOrder.playground/Contents.swift
+++ 102-LevelOrder.playground/Contents.swift
@@ -12,10 +12,10 @@ class TreeNode {
 }
 
 
-class Solution {
+class Solution1 {
+    // 此算法直观暴力时间杂度 O(n)，空间 O(n)
     func levelOrder(_ root: TreeNode?) -> [[Int]] {
         guard let r = root else { return [] }
-        
         
         var result = [[Int]]()
         
@@ -30,13 +30,12 @@ class Solution {
         var curLevelResult = [Int]()
         var nextLevelNodes = [TreeNode]()
         for node in nodes {
+            curLevelResult.append(node.val)
             if let l = node.left {
                 nextLevelNodes.append(l)
-                curLevelResult.append(l.val)
             }
             if let r = node.right {
                 nextLevelNodes.append(r)
-                curLevelResult.append(r.val)
             }
         }
         result.append(curLevelResult)
@@ -44,3 +43,56 @@ class Solution {
         appendLevelNode(result: &result, nodes: nextLevelNodes)
     }
 }
+
+class Solution2 {
+    // 总体思路与1相同，只是用迭代取代了递归，且无需每层创建一个数组
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        var result = [[Int]]()
+        guard let r = root else { return result }
+        
+        var curLevelNodes = [r]
+        
+        while !curLevelNodes.isEmpty {
+            var curLevelVals = [Int]()
+            for _ in 0..<curLevelNodes.count {
+                let node = curLevelNodes.removeFirst()
+                curLevelVals.append(node.val)
+                if let l = node.left {
+                    curLevelNodes.append(l)
+                }
+                if let r = node.right {
+                    curLevelNodes.append(r)
+                }
+            }
+            result.append(curLevelVals)
+        }
+        return result
+    }
+}
+
+class Solution3 {
+    // 这才是真正理解递归，将重复内容缩减至最少。
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        var result = [[Int]]()
+        guard let r = root  else {
+            return result
+        }
+        levelOrder(result: &result, node: r, depth: 1)
+        return result
+    }
+    
+    func levelOrder(result: inout [[Int]], node: TreeNode?, depth: Int) {
+        guard let n = node else {
+            return
+        }
+        
+        if result.count < depth {
+            result.append([Int]())
+        }
+        result[depth - 1].append(n.val)
+        levelOrder(result: &result, node: n.left, depth: depth + 1)
+        levelOrder(result: &result, node: n.right, depth: depth + 1)
+    }
+}
+
+
