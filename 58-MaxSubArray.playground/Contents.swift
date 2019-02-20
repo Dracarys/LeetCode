2a0b4595b6a206232bbf31496fdbdb081df1e7f8
Dracarys <cavalry001@hotmail.com>
2019-02-20 16:58:11 +0800

    New solution

--- 58-MaxSubArray.playground/Contents.swift
+++ 58-MaxSubArray.playground/Contents.swift
@@ -1,10 +1,18 @@
 import Cocoa
 
-//!!!!: 理解了流程，但是没理解“证明过程”。
+/**
+ * 思路：
+ *     假设数组中只有一个元素，那么就是nums[0]
+ *     假设数组中有两个元素，那么就是max(nums[0], nums[0] + nums[1])
+ *     以此类推类推，结果为：max(maxSubArray[n-1], maxSubArray[n-1] + nums[n])
+ *     考虑到整数包括负数，所以：初始和为0，即全部都是负数，一个也不选，[]
+ *     这里的描述不准确，有待更新。
+ */
+
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
         guard !nums.isEmpty else { return 0 }
-        var rest = nums[0]
+        var final = nums.first!
         var sum = 0
         nums.forEach { num in
             if sum > 0 {
@@ -12,9 +20,9 @@ class Solution {
             } else {
                 sum = num
             }
-            rest = max(rest, sum)
+            final = max(final, sum)
         }
-        return rest
+        return final
     }
 }
 
