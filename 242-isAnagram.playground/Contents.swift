48efdbf69328ea0f794deec67e0f7cbfe4f23531
Dracarys <cavalry001@hotmail.com>
2019-02-21 16:57:07 +0800

    New solution

---
+++ 242-isAnagram.playground/Contents.swift
@@ -0,0 +1,46 @@
+import Cocoa
+
+// 种类、个数均相同即为异位词。
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard !s.isEmpty && !t.isEmpty else { return true }
+        guard s.count == t.count else  { return false }
+        let arr1 = Array(s)
+        let arr2 = Array(t)
+        var dic1 = [Character: Int]()
+        var dic2 = [Character: Int]()
+        for i in 0..<arr1.count {
+            if !dic1.keys.contains(arr1[i]) {
+                dic1[arr1[i]] = 1
+            } else {
+                dic1[arr1[i]]! += 1
+            }
+            if !dic2.keys.contains(arr2[i]) {
+                dic2[arr2[i]] = 1
+            } else {
+                dic2[arr2[i]]! += 1
+            }
+        }
+        
+        for key in dic1.keys {
+            if dic1[key] != dic2[key] { return false }
+        }
+        return true
+    }
+}
+
+// 此算法逻辑简单，但是时间杂度增加了
+class Solution1 {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard !s.isEmpty && !t.isEmpty else { return true }
+        guard s.count == t.count else  { return false }
+        let arr1 = Array(s).sorted()
+        let arr2 = Array(t).sorted()
+        
+        return arr1 == arr2
+    }
+}
+
+let s = "anagram", t = "nagaram"
+let solution = Solution()
+solution.isAnagram(s, t)
