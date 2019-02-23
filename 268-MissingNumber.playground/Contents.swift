eaca6ca4b8f3fe1fd643af4ae72cbc7abfd0c3da
William <cavalry001@hotmail.com>
2019-02-23 17:02:51 +0800

    New solution

--- 268-MissingNumber.playground/Contents.swift
+++ 268-MissingNumber.playground/Contents.swift
@@ -20,7 +20,27 @@ class Solution1 {
     }
 }
 
+class Solution2 {
+    func missingNumber(_ nums: [Int]) -> Int {
+        let n = nums.count
+        let sum = nums.reduce(0, +)
+        return n * ( 1 + n) / 2 - sum
+    }
+}
 
-let nums = [0, 1, 2, 4, 5, 6]
-let s = Solution1()
+// 该方法相对上面的方法，优点在于不会出现益处问题。
+class Solution3 {
+    func missingNumber(_ nums: [Int]) -> Int {
+        var missingNumber = 0
+        for i in 0..<nums.count {
+            missingNumber ^= (i + 1) ^ nums[i]
+        }
+        return missingNumber
+
+    }
+}
+
+
+let nums = [0, 1, 2, 3, 4, 5, 6]
+let s = Solution2()
 s.missingNumber(nums)
