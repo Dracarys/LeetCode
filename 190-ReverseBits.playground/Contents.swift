5622fc2b7d15e91823fd8def34232ead11b7195f
Dracarys <cavalry001@hotmail.com>
2019-02-25 15:41:28 +0800

    New solution

---
+++ 190-ReverseBits.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+class Solution {
+    
+    // 别人的算法，组成原理还需要再复习。
+    func reverseBits(_ n: UInt32) -> UInt32 {
+        var originNum = n
+        var result: UInt32 = 0
+        var steps = 32
+        while  steps > 0 {
+            result <<= 1
+            result += originNum & 1
+            originNum >>= 1
+            
+            steps -= 1
+        }
+        return result
+    }
+}
+
+Solution().reverseBits(1)
+Solution().reverseBits(2147483648)
+
