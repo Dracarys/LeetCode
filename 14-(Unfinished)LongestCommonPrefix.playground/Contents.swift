ee93e386402f900d5d962ce172938323011f9601
Dracarys <cavalry001@hotmail.com>
2019-02-26 10:28:38 +0800

    New question

---
+++ 14-(Unfinished)LongestCommonPrefix.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        guard strs.count > 2 else { return "" }
+        
+        var prefix = strs.first!
+        
+        
+    }
+}
