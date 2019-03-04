a5265de527c1ada0c714f68318f38f9577895f56
Dracarys <cavalry001@hotmail.com>
2019-03-04 12:25:54 +0800

    Add comments

--- 15-ThreeSum.playground/Contents.swift
+++ 15-ThreeSum.playground/Contents.swift
@@ -3,26 +3,30 @@ import Cocoa
 // 非原创
 class Solution {
     func threeSum(_ nums: [Int]) -> [[Int]] {
-        let numsSotred = nums.sorted()
-        
         var results = [[Int]]()
+        guard nums.count > 2  else {
+            return results
+        }
+        
+        let numsSorted = nums.sorted()
         var i = 0
-        while i < numsSotred.count {
-            let num = numsSotred[i]
+        while i < numsSorted.count && numsSorted[i] <= 0 {
+            let num = numsSorted[i]
             var head = i + 1
-            var tail = numsSotred.count - 1
+            var tail = numsSorted.count - 1
             
+            // 从余下的序列中查询符合条件的数值
             while head < tail {
-                let sum = num + numsSotred[head] + numsSotred[tail]
+                let sum = num + numsSorted[head] + numsSorted[tail]
                 if sum == 0 {
-                    results.append([num, numsSotred[head], numsSotred[tail]])
+                    results.append([num, numsSorted[head], numsSorted[tail]])
                     head += 1
                     tail -= 1
-                    // 跳过重复
-                    while (head < tail && numsSotred[head] == numsSotred[head - 1]) {
+                    // 跳过重复的数值
+                    while head < tail && numsSorted[head] == numsSorted[head - 1] {
                         head += 1
                     }
-                    while (head < tail && numsSotred[tail] == numsSotred[tail + 1]) {
+                    while head < tail && numsSorted[tail] == numsSorted[tail + 1] {
                         tail -= 1
                     }
                 } else if sum > 0 {
@@ -32,8 +36,8 @@ class Solution {
                 }
             }
             i += 1
-            //跳过重复
-            while i < numsSotred.count && numsSotred[i] == numsSotred[i - 1] {
+            // 跳过重复的数值
+            while i < numsSorted.count && numsSorted[i] == numsSorted[i - 1] {
                 i += 1
             }
         }
@@ -44,7 +48,7 @@ class Solution {
 
 // Test
 
-let nums = [-1, 0, 1, 2, -1, -4]
+let nums = [0, 0, 0]
 let s = Solution()
 let r = s.threeSum(nums)
 
