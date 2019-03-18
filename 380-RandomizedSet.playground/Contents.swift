cd3238d835d37a17d789afd61cb4d0edeaf8abf3
Dracarys <cavalry001@hotmail.com>
2019-03-18 16:50:59 +0800

    New solution

---
+++ 380-RandomizedSet.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Cocoa
+
+class RandomizedSet {
+    var map: [Int: Int]
+    
+    /** Initialize your data structure here. */
+    init() {
+        self.map = [Int: Int]()
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    func insert(_ val: Int) -> Bool {
+        if map[val] != nil {
+            map[val] = val
+            return false
+        }
+        return true
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    func remove(_ val: Int) -> Bool {
+        if map[val] != nil {
+            map.removeValue(forKey: val)
+            return true
+        }
+        return false
+    }
+    
+    /** Get a random element from the set. */
+    func getRandom() -> Int {
+        let allKeys = map.keys
+        let index = Int.random(in: 0..<allKeys.count)
+        let randomKey = allKeys[index]
+        return map[randomKey]!
+    }
+}
