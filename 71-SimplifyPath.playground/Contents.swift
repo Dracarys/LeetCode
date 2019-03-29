5ae89bd51523e4210b8d9816d4c601e471e453b0
Dracarys <cavalry001@hotmail.com>
2019-03-29 15:54:23 +0800

    update solution

--- 71-SimplifyPath.playground/Contents.swift
+++ 71-SimplifyPath.playground/Contents.swift
@@ -14,11 +14,30 @@ class Solution {
             }
         }
         
+        if result.count == 0 {
+            return "/"
+        }
         return result.reduce(""){ $0 + "/" + $1 }
     }
 }
 
-let path = "/a//b////c/d//././/.."
-let s = Solution()
+// 此种方式还不如上面的快呢。
+class Solution1 {
+    func simplifyPath(_ path: String) -> String {
+        let strs = path.components(separatedBy: "/")
+        var result = [String]()
+        strs.forEach { (s) in
+            if s == ".." {
+                result.popLast()
+            } else if !s.isEmpty && s != "." {
+                result.append(s)
+            }
+        }
+        return "/" + result.joined(separator: "/")
+    }
+}
+
+let path = "/home/"
+let s = Solution1()
 
 s.simplifyPath(path)
